b084e93b602daa6e99e0104e75a15a3c684d143e
zGIKS <166082577+zGIKS@users.noreply.github.com>
2025-07-18 21:59:38 -0500

    Add date formatting function and alignment enums to style.rs

--- src/style.rs
+++ src/style.rs
@@ -1,3 +1,35 @@
+
+
+/// Returns a formatted date string for the given chrono::DateTime.
+pub fn format_date<T>(dt: &chrono::DateTime<T>) -> String
+where
+    T: chrono::TimeZone,
+    T::Offset: std::fmt::Display,
+{
+    dt.format("%A, %d %B %Y").to_string()
+}
+#[allow(dead_code)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum HorizontalAlign {
+    Left,
+    Center,
+    Right,
+}
+
+#[allow(dead_code)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum VerticalAlign {
+    Top,
+    Center,
+    Bottom,
+}
+
+#[allow(dead_code)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum HourFormat {
+    H24,
+    H12,
+}
 // style.rs
 // List of available digit styles for the clock
 
